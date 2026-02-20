7a195b3b8a9e451017bbbb54d6b7164b57844ab9
Sam Robertson <samrobertson5@outlook.com>
2026-02-20 16:29:03 +0000

    item class

---
+++ sellersbookkeepingapp/lib/Classes/item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class Item {
+  final String name;
+  final String description;
+  String boughtFrom = '';
+  DateTime boughtDate;
+
+  bool isSold = false;
+  double sellingPrice = 0.0;
+  double costPrice = 0.0;
+  double soldPrice = 0.0;
+  DateTime? soldDate;
+  int? daysToSell = 0;
+
+  double get profit => soldPrice - costPrice;
+
+  Item({
+    required this.name,
+    required this.description,
+    this.boughtFrom = '',
+    this.sellingPrice = 0.0,
+    this.costPrice = 0.0,
+    this.soldPrice = 0.0,
+    required this.boughtDate,
+    this.soldDate,
+  });
+
+  dynamic soldItem() {
+    if (!isSold) {
+      soldPrice = sellingPrice;
+      isSold = true;
+      soldDate = DateTime.now();
+      daysToSell = soldDate!.difference(boughtDate).inDays;
+    }
+  }
+
+  changeSellingPrice(double newPrice) {
+   if (!isSold) {
+    if (newPrice <= 0) {
+      // Show a warning dialog or message to the user
+      print('Error: Selling price must be greater than zero.');
+    }
+    else {
+      sellingPrice = newPrice;
+    }
+   }
+   else {
+    print('Error: Cannot change selling price of a sold item.');
+   }
+  }
+}
